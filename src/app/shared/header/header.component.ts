0d010814a40698b3aca3dc7138a92ac57ed3145a
Carlos Zabala <czabala847@gmail.com>
2022-07-10 17:51:29 -0500

    Cambiando color header al hacer scroll

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  scroll: boolean = false;
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    if (window.pageYOffset > 0) {
+      this.scroll = true;
+    } else {
+      this.scroll = false;
+    }
+  }
+}
